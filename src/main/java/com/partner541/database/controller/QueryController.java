7cf6ee0528bd7a47212c0683ea373100e1a46e93
xue <1758966306@qq.com>
2018-09-14 00:26:23 +0800

    Merge branch 'master' of github.com:q474890522/DBMS
    # Conflicts:
    #	sqlLog.log

--- src/main/java/com/partner541/database/controller/QueryController.java
+++ src/main/java/com/partner541/database/controller/QueryController.java
@@ -1,6 +1,12 @@
 package com.partner541.database.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.partner541.database.model.Cell;
+import com.partner541.database.model.PRBpor;
+import com.partner541.database.service.CellService;
+import com.partner541.database.service.KPIService;
+import com.partner541.database.service.PRBService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +16,25 @@ import java.util.*;
 
 @Controller
 public class QueryController {
+    @Autowired
+    CellService cellService;
+
+    @Autowired
+    KPIService kpiService;
+
+    @Autowired
+    PRBService prbService;
+
+
+    public String SECTOR_ID, SECTOR_NAME, ENODEBID, ENODEB_NAME;
+
     @RequestMapping(path = "/cellnewdata")
     @ResponseBody
     public Object cellnewdata(@RequestBody JSONObject params){
-        String SECTOR_Id = params.getString("SECTOR_ID");
-        System.out.println(SECTOR_Id);
+        SECTOR_ID = params.getString("SECTOR_ID");
+        SECTOR_NAME = params.getString("SECTOR_NAME");
+        ENODEBID = params.getString("ENODEBID");
+        ENODEB_NAME = params.getString("ENODEB_NAME");
 
         Map<String,Object> map = new HashMap<>();
         map.put("success",true);
@@ -24,10 +44,7 @@ public class QueryController {
     @RequestMapping(path = "/celldata")
     @ResponseBody
     public Object celldata() {
-        List<person> list = new LinkedList<>();
-
-        list.add(new person("user1","password1"));
-        list.add(new person("user2","password2"));
+        List<Cell> list = null;
 
 
         Map<String,Object> map = new HashMap<>();
@@ -39,14 +56,23 @@ public class QueryController {
         return map;
     }
 
+
+    public String NENAME, parameter;
+    public Date kpibegintime, kpiendtime;
     @RequestMapping(path = "/kpinewdata")
     @ResponseBody
     public Object kpinewdata(@RequestBody JSONObject params) {
-        String kpibegintime = params.getString("KPIbegintime");
-        Date kpiendtime   = params.getDate("KPIendtime");
+        NENAME = params.getString("NENAME");
+        parameter = params.getString("properties");
+        kpibegintime = params.getDate("KPIbegintime");
+        kpiendtime   = params.getDate("KPIendtime");
 
+        System.out.println(NENAME);
+        System.out.println(parameter);
         System.out.println(kpibegintime);
         System.out.println(kpiendtime);
+
+        kpiService.selectRPC_CONN_RESET_RATE();
 
         Map<String,Object> map = new HashMap<>();
         map.put("success",true);
@@ -57,14 +83,28 @@ public class QueryController {
     @RequestMapping(path = "/prbnewdata")
     @ResponseBody
     public Object prbnewdata(@RequestBody JSONObject params) {
-        String prbbegintime = params.getString("PRBbegintime");
+        String NENAME = params.getString("NENAME");
+        String properties = params.getString("properties");
+        long timesize = params.getLong("timesize");
+        Date prbbegintime = params.getDate("PRBbegintime");
         Date prbendtime = params.getDate("PRBendtime");
 
-        System.out.println(prbbegintime);
-        System.out.println(prbendtime);
+        System.out.println(timesize);
+
+        //List<PRBpor> list = prbService.getXPRB(NENAME,prbbegintime,prbendtime,timesize,properties);
+
+        List<PRBpor> list = new LinkedList<>();
+        PRBpor temp1 = new PRBpor();
+        PRBpor temp2 = new PRBpor();
+        temp1.setX(8);
+        temp2.setX(10);
+        temp1.setSTARTTIME(prbbegintime);
+        temp2.setSTARTTIME(prbendtime);
+        list.add(temp1);
+        list.add(temp2);
 
         Map<String,Object> map = new HashMap<>();
-        map.put("success",true);
+        map.put("data",list);
         return map;
     }
 
