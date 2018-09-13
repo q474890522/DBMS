75a25b3c23951a10657596ff798715bb626dc797
sjw <sjww1997@gmail.com>
2018-09-13 14:05:19 +0800

    static htmls

--- src/main/java/com/partner541/database/controller/QueryController.java
+++ src/main/java/com/partner541/database/controller/QueryController.java
@@ -1,4 +1,12 @@
 package com.partner541.database.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class QueryController {
+    @RequestMapping("/query")
+    public String demo() {
+        return "Query";
+    }
 }
--- src/main/java/com/partner541/database/controller/SignInController.java
+++ src/main/java/com/partner541/database/controller/SignInController.java
@@ -1,10 +1,30 @@
 package com.partner541.database.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class SignInController {
+    @RequestMapping("/signin/update")
+    @ResponseBody
+    public Object siginupdate(@RequestBody JSONObject params) {
+        String username = params.getString("username");
+        String password = params.getString("password");
+
+        /*
+        * 密码和用户名正确
+        * */
+        Map<String,Object> map = new HashMap<>();
+        map.put("success",true);
+        return map;
+    }
+
     @RequestMapping("/")
     public String say(){
         return "SignIn";
